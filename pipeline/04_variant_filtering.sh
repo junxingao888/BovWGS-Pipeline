6e1ad10e095c97e4a15d41a9201328891b127d6d
Junxin Gao <123380560+junxingao888@users.noreply.github.com>
2026-03-11 20:09:31 +0100

    Add comments for variant filtering script
    Added comments explaining the variant filtering process using VCFtools.

--- pipeline/04_variant_filtering.sh
+++ pipeline/04_variant_filtering.sh
@@ -1,1 +1,9 @@
-1
+# Variant filtering
+
+#Variant filtering was performed using VCFtools (v0.1.16).
+vcftools --gzvcf ${SAMPLE}.vcf.gz \
+         --maf 0.05 \
+         --min-meanDP 4 \
+         --max-missing 0.95 \
+         --recode --recode-INFO-all \
+         --out ${SAMPLE}_filtered
