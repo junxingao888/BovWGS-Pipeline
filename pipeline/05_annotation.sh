c880823a27691d48ab3dfa0e4c4b1b05a8503a6e
Junxin Gao <123380560+junxingao888@users.noreply.github.com>
2026-03-11 20:10:24 +0100

    Implement variant annotation using Ensembl VEP
    Added functional annotation of variants using VEP.

--- pipeline/05_annotation.sh
+++ pipeline/05_annotation.sh
@@ -1,1 +1,18 @@
-1
+# Variant annotation
+# Functional annotation of variants was performed using Ensembl Variant Effect Predictor (VEP, release 111).
+
+VEP_CACHE_DIR=/path/to/vep_cache
+REFERENCE_FASTA=/path/to/Bos_taurus.ARS-UCD1.2.dna.toplevel.fa.gz
+
+vep -i ${SAMPLE}.vcf.gz \
+    -o ${SAMPLE}_annotation.vcf.gz \
+    --vcf \
+    --compress_output bgzip \
+    --force_overwrite \
+    --cache \
+    --offline \
+    --species bos_taurus \
+    --assembly ARS-UCD1.2 \
+    --dir_cache ${VEP_CACHE_DIR} \
+    --fasta ${REFERENCE_FASTA} \
+    --fork 8
