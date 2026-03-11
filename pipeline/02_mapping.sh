79e48a0de5ce06445465b207ad1e2487289ef1f6
Junxin Gao <123380560+junxingao888@users.noreply.github.com>
2026-03-11 20:03:56 +0100

    Update 02_mapping.sh

--- pipeline/02_mapping.sh
+++ pipeline/02_mapping.sh
@@ -1,1 +1,5 @@
-1
+# Read mapping
+# Reads were aligned to the Bos taurus reference genome (ARS-UCD1.2) using BWA-MEM2 (v2.2.1).
+bwa-mem2 index ${REF}
+
+bwa-mem2 mem ${REF} ${READS_1} ${READS_2} > ${SAMPLE}.sam
